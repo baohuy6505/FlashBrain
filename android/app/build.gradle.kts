7420a71aee636bc5eb82335be7d7e40f73b17847
BaoHuy <baohuy6505@gmail.com>
2026-04-14 09:33:26 +0700

    fix(config): adjust network security config and manifest permissions for docker api

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,7 +76,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-
+// 5. ViewModel, Coroutines, UI Tools
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // THÊM DÒNG NÀY ĐỂ HẾT GẠCH NGANG VÀ CHẠY ĐƯỢC ON_RESUME
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
     // 1. Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
